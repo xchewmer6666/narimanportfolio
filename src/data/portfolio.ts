da9a02a5dc59a6862c86151ccdc4ed28fbdb8c3a
xchewmer6666 <xchewmer6666@gmail.com>
2023-09-10 16:15:38 +0330

    portfolio_mockups_added

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,18 +1,24 @@
 import jz from "../assets/jordanzone.png";
 import mn from "../assets/minecraft.png";
+import mc from "../assets/mockups.png";
 
 export default [
   {
     title: "Jordanzone",
     imgUrl: jz,
-    stack: ["HTML", "CSS", "JavaScript", "React Js"],
+    stack: ["HTML", "CSS", "JavaScript", "React"],
     link: "https://jordanzone.ir",
   },
   {
     title: "Minecraft Clone",
     imgUrl: mn,
-    stack: ["HTML", "CSS", "JavaScript", "React Js", "Zustand"],
+    stack: ["HTML", "CSS", "JavaScript", "React", "Zustand"],
     link: "https://minecraftclone.onrender.com",
+  },
+  {
+    title: "Mockup Container(aggregation of mockups)",
+    imgUrl: mc,
+    stack: ["React", "Vue", "Next|Nuxt", "Svelte Kit"],
   },
   {
     title: "ctOS_Chat App(work in progress)",
